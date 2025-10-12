cb00e91fdd510d4b9950df871a6e626927ba8234
HEHAOLL <879084908@qq.com>
2025-10-12 21:49:05 +0800

    second

--- Project4/FileNodeType.h
+++ Project4/FileNodeType.h
@@ -7,9 +7,8 @@
 #include <cctype> 
 enum class FileNodeType
 {
-    DIRECTORY,
-    TXT,
-    ERROR
+    DIRECTORY, TXT, RTF, DOC, DOCX, XLS, XLSX, PPT, PPTX, PDF, JNG, GIF, BMP, PSD, SVG, MP3, WAV, FLAC, M4A, OGG, MP4, AVI, MKV, FLV, MOV, RMVB,
+    EXE, DMG, APK, DEB, SH, ZIP, RAR, IOS, JAVA, CPP, CJ, C, CS, PY, JS, TS, PHP, RB, GO, RS, SWIFT, KT, PL, PM, R, RMD, ERROR
 };
 
 // º¯ÊýÉùÃ÷
